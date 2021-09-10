911c095dd06434a8fe062e1ea9c4ecf42463bf25
David Lukens <davidqcy02@gmail.com>
2021-09-10 14:51:18 -0500

    fix ui-box height

--- client/src/components/MainGUI.tsx
+++ client/src/components/MainGUI.tsx
@@ -7,8 +7,18 @@ export default function MainGUI() {
         <div className="flex-container-column">
             <TopBar />
             <div className="ui-row">
-                <div style={{ backgroundColor: 'pink' }}>Hello</div>
-                <div style={{ backgroundColor: 'yellow' }}>How Are You</div>
+                <div className="ui-box" style={{ backgroundColor: 'pink' }}>
+                    <div>Hello</div>
+                    <div>dear friend</div>
+                </div>
+                <div className="ui-box" style={{ backgroundColor: 'violet' }}>
+                    <div>How are you</div>
+                    <div>dear friend</div>
+                </div>
+                <div className="ui-box" style={{ backgroundColor: 'turquoise', width: '100%' }}>
+                    <div>This is working</div>
+                    <div>dear friend</div>
+                </div>
             </div>
         </div>
     )
