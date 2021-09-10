82f03d8d5690a30561e0ec5d82648d313a17afa2
David Lukens <davidqcy02@gmail.com>
2021-09-10 14:29:52 -0500

    flex rows and columns are working

--- client/src/components/MainGUI.tsx
+++ client/src/components/MainGUI.tsx
@@ -1,8 +1,15 @@
 
+import '../styles.scss'
+import TopBar from './TopBar'
+
 export default function MainGUI() {
     return (
-        <div>
-            Main GUI
+        <div className="flex-container-column">
+            <TopBar />
+            <div className="ui-row">
+                <div style={{ backgroundColor: 'pink' }}>Hello</div>
+                <div style={{ backgroundColor: 'yellow' }}>How Are You</div>
+            </div>
         </div>
     )
 }
