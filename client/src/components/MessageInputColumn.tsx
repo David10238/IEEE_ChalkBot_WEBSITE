e9ccf6776542ea011a100c1aebbd36bdead366a2
David Lukens <davidqcy02@gmail.com>
2021-09-13 10:57:54 -0500

    add input box for messages

--- client/src/components/MessageInputColumn.tsx
+++ client/src/components/MessageInputColumn.tsx
@@ -1,9 +1,36 @@
 
+function handleSubmission() {
+    // use any type as it simplefies grabbing value
+    const textbox: any = document.getElementById("message-input-box")!!
+    let text: string = textbox.value
+
+    // remove whitespace
+    text = text.split(/\s+/).join('')
+
+    if (text.length === 0) {
+        console.log("text is emtpy")
+        return
+    }
+
+    //must be alpha-numeric
+    const regex = RegExp("^[a-zA-Z0-9]*$")
+    if (!regex.test(text)) {
+        console.log("text is not alpha-numeric")
+        return
+    }
+
+    console.log(text)
+}
+
 export default function MessageInputColumn() {
     return (
         <div id="message-input-column">
-            <div>This column</div>
-            <div>Contains information about motors</div>
+            <div className="fill-width fill-height">
+                <textarea id="message-input-box" />
+            </div>
+            <div className="fill-width">
+                <button id="message-button" onClick={handleSubmission}>Run</button>
+            </div>
         </div>
     )
 }
